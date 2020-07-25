0c4298f1377ed65310fe838dd1cb3406ac1b8907
Jack Hanson <jhanson1384@gmail.com>
2020-07-24 21:00:25 -0500

    Added Constructor for PlayerCharacter class

---
+++ app/src/main/java/com/jhanson1384/bahothassistant/CharacterEnum.java
@@ -0,0 +1,16 @@
+package com.jhanson1384.bahothassistant;
+
+public enum CharacterEnum {
+    MISSY,
+    ZOE,
+    JENNY,
+    HEATHER,
+    ZOSTRA,
+    VIVIAN,
+    DARRIN,
+    OX,
+    BRANDON,
+    PETER,
+    RHINEHARDT,
+    LONGFELLOW
+}
