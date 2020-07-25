5d320e37704d1f85dbc0763debe79b66c74efc34
Jack Hanson <jhanson1384@gmail.com>
2020-07-25 10:36:07 -0500

    Refactored CharacterEnum to contain index field for accessing characters from array

--- app/src/main/java/com/jhanson1384/bahothassistant/CharacterEnum.java
+++ app/src/main/java/com/jhanson1384/bahothassistant/CharacterEnum.java
@@ -1,16 +1,27 @@
 package com.jhanson1384.bahothassistant;
 
 public enum CharacterEnum {
-    MISSY,
-    ZOE,
-    JENNY,
-    HEATHER,
-    ZOSTRA,
-    VIVIAN,
-    DARRIN,
-    OX,
-    BRANDON,
-    PETER,
-    RHINEHARDT,
-    LONGFELLOW
+    MISSY(0),
+    ZOE(1),
+    JENNY(2),
+    HEATHER(3),
+    ZOSTRA(4),
+    VIVIAN(5),
+    DARRIN(6),
+    OX(7),
+    BRANDON(8),
+    PETER(9),
+    RHINEHARDT(10),
+    LONGFELLOW(11);
+
+    //Used as a key to find specific character in array
+    private final int index;
+
+    CharacterEnum(int index){
+        this.index = index;
+    }
+
+    public int getIndex(){
+        return index;
+    }
 }
--- app/src/main/java/com/jhanson1384/bahothassistant/ChooseCharacterActivity.java
+++ app/src/main/java/com/jhanson1384/bahothassistant/ChooseCharacterActivity.java
@@ -18,30 +18,30 @@ public class ChooseCharacterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_choose_character);
 
         //Initialize array containing all characters
-        init_all_characters();
+        initAllCharacters();
         //Populate available_characters list, initially contains all characters
-        init_avail_characters();
+        initAvailCharacters();
     }
 
-    private void init_all_characters(){
+    private void initAllCharacters(){
         //There are 12 total characters
         all_characters = new PlayerCharacter[12];
         //Populate array with one instance of each character
-        all_characters[0] = new PlayerCharacter(CharacterEnum.MISSY);
-        all_characters[1] = new PlayerCharacter(CharacterEnum.ZOE);
-        all_characters[2] = new PlayerCharacter(CharacterEnum.JENNY);
-        all_characters[3] = new PlayerCharacter(CharacterEnum.HEATHER);
-        all_characters[4] = new PlayerCharacter(CharacterEnum.ZOSTRA);
-        all_characters[5] = new PlayerCharacter(CharacterEnum.VIVIAN);
-        all_characters[6] = new PlayerCharacter(CharacterEnum.DARRIN);
-        all_characters[7] = new PlayerCharacter(CharacterEnum.OX);
-        all_characters[8] = new PlayerCharacter(CharacterEnum.BRANDON);
-        all_characters[9] = new PlayerCharacter(CharacterEnum.PETER);
-        all_characters[10] = new PlayerCharacter(CharacterEnum.RHINEHARDT);
-        all_characters[11] = new PlayerCharacter(CharacterEnum.LONGFELLOW);
+        all_characters[CharacterEnum.MISSY.getIndex()] = new PlayerCharacter(CharacterEnum.MISSY);
+        all_characters[CharacterEnum.ZOE.getIndex()] = new PlayerCharacter(CharacterEnum.ZOE);
+        all_characters[CharacterEnum.JENNY.getIndex()] = new PlayerCharacter(CharacterEnum.JENNY);
+        all_characters[CharacterEnum.HEATHER.getIndex()] = new PlayerCharacter(CharacterEnum.HEATHER);
+        all_characters[CharacterEnum.ZOSTRA.getIndex()] = new PlayerCharacter(CharacterEnum.ZOSTRA);
+        all_characters[CharacterEnum.VIVIAN.getIndex()] = new PlayerCharacter(CharacterEnum.VIVIAN);
+        all_characters[CharacterEnum.DARRIN.getIndex()] = new PlayerCharacter(CharacterEnum.DARRIN);
+        all_characters[CharacterEnum.OX.getIndex()] = new PlayerCharacter(CharacterEnum.OX);
+        all_characters[CharacterEnum.BRANDON.getIndex()] = new PlayerCharacter(CharacterEnum.BRANDON);
+        all_characters[CharacterEnum.PETER.getIndex()] = new PlayerCharacter(CharacterEnum.PETER);
+        all_characters[CharacterEnum.RHINEHARDT.getIndex()] = new PlayerCharacter(CharacterEnum.RHINEHARDT);
+        all_characters[CharacterEnum.LONGFELLOW.getIndex()] = new PlayerCharacter(CharacterEnum.LONGFELLOW);
     }
 
-    private void init_avail_characters(){
+    private void initAvailCharacters(){
         available_characters = new ArrayList<>();
         for (PlayerCharacter pc : all_characters){
             available_characters.add(pc);
