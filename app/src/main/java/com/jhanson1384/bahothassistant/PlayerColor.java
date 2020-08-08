4912095353e766628f068e14d683237bea2a34c0
Jack Hanson <jhanson1384@gmail.com>
2020-08-08 18:21:51 -0500

    Added integer conversion methods to PlayerColor to facilitate sending via Intent

--- app/src/main/java/com/jhanson1384/bahothassistant/PlayerColor.java
+++ app/src/main/java/com/jhanson1384/bahothassistant/PlayerColor.java
@@ -6,12 +6,39 @@ import android.view.View;
 //Player color determines which characters are available to pick from the start of the game
 //Each player must have a different color character
 public enum PlayerColor {
-    YELLOW,
-    PURPLE,
-    BLUE,
-    RED,
-    GREEN,
-    WHITE;
+    NONE(0),
+    YELLOW(1),
+    PURPLE(2),
+    BLUE(3),
+    RED(4),
+    GREEN(5),
+    WHITE(6);
+
+    private final int value;
+
+    PlayerColor(int val) { this.value = val; }
+
+    public int getValue(){ return value; }
+
+    //Decode PlayerColor from int value (used when sending via Intent)
+    public PlayerColor decodeValue(int val){
+        switch(val){
+            case 1:
+                return YELLOW;
+            case 2:
+                return PURPLE;
+            case 3:
+                return BLUE;
+            case 4:
+                return RED;
+            case 5:
+                return GREEN;
+            case 6:
+                return WHITE;
+            default:
+                return NONE;
+        }
+    }
 
     public void setBgColor(View v){
         switch(this){
