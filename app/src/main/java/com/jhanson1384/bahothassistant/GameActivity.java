af2ef9cbda2ac0f15714b46c178f08ddd1eb79ee
Jack Hanson <jhanson1384@gmail.com>
2020-08-17 21:08:21 -0500

    Changed GameActivity to adding character display fragment programmatically (compared to inside XML layout)

--- app/src/main/java/com/jhanson1384/bahothassistant/GameActivity.java
+++ app/src/main/java/com/jhanson1384/bahothassistant/GameActivity.java
@@ -1,12 +1,15 @@
 package com.jhanson1384.bahothassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
+    private FragmentManager frag_manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,13 @@ public class GameActivity extends AppCompatActivity {
         CharacterEnum char_type = CharacterEnum.decodeIndex(
                 i.getIntExtra(ChooseCharacterActivity.EXTRA_CHAR_TYPE, 0));
         game = new BahothGame(char_type);
+        //Init fragment manager
+        frag_manager = getSupportFragmentManager();
+        //Setup initial character display layout
+        FragmentTransaction transaction = frag_manager.beginTransaction();
+        DisplayCharacterFragment disp_char_frag = new DisplayCharacterFragment();
+        transaction.add(R.id.game_activity, disp_char_frag);
+        transaction.commit();
     }
 
     private BahothGame game;
