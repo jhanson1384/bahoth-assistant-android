689eef3ca69b0dde66c40bca16c57301f490a1c2
Jack Hanson <jhanson1384@gmail.com>
2020-09-13 13:12:25 -0500

    Created OnClickListener for Confirm Character button

--- app/src/main/java/com/jhanson1384/bahothassistant/ConfirmCharacterFragment.java
+++ app/src/main/java/com/jhanson1384/bahothassistant/ConfirmCharacterFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ConfirmCharacterFragment extends Fragment {
 
@@ -19,6 +21,19 @@ public class ConfirmCharacterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_confirm_character, container, false);
+        View v = inflater.inflate(R.layout.fragment_confirm_character, container, false);
+
+        //Set onClick behavior for Confirm Character button
+        Button btn = (Button) v.findViewById(R.id.confirm_character_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Button clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return v;
     }
+
+    
 }
