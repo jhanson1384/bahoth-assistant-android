7c5f1c162d6874de8264dbcef965b80d54daddff
Jack Hanson <jhanson1384@gmail.com>
2020-09-28 18:40:25 -0500

    Added dice images and function mapping dice value to correct image resource

--- app/src/main/java/com/jhanson1384/bahothassistant/DiceRollFragment.java
+++ app/src/main/java/com/jhanson1384/bahothassistant/DiceRollFragment.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class DiceRollFragment extends Fragment {
+    private DiceManager diceManager;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,24 @@ public class DiceRollFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dice_roll, container, false);
+        View v = inflater.inflate(R.layout.fragment_dice_roll, container, false);
+
+        //Init dice manager
+        diceManager = new DiceManager(8);
+
+        return v;
+    }
+
+    //Get resource ID for dice image of given value
+    private int getDiceImgID(int dice_val){
+        switch (dice_val){
+            case 0:
+                return R.drawable.zero_die;
+            case 1:
+                return R.drawable.one_die;
+            case 2:
+                return R.drawable.two_die;
+        }
+        return R.drawable.zero_die;
     }
 }
