665c3beea7fda0891b9adfd97f91ff04d6734b5f
Jack Hanson <jhanson1384@gmail.com>
2020-09-30 19:54:54 -0500

    Implemented increment/decrement buttons to adjust number of dice

--- app/src/main/java/com/jhanson1384/bahothassistant/NDiceRollFragment.java
+++ app/src/main/java/com/jhanson1384/bahothassistant/NDiceRollFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class NDiceRollFragment extends DiceRollFragment {
@@ -24,12 +25,29 @@ public class NDiceRollFragment extends DiceRollFragment {
         View choose_n_dice_container = inflater.inflate(R.layout.choose_n_dice, v, false);
         v.addView(choose_n_dice_container, 0);
         displayNDice((TextView) choose_n_dice_container.findViewById(R.id.n_dice_label));
+        //Set OnClickListener for increment/decrement buttons
+        Button incr_n_dice_btn = (Button) choose_n_dice_container.findViewById(R.id.incr_n_dice_btn);
+        Button decr_n_dice_btn = (Button) choose_n_dice_container.findViewById(R.id.decr_n_dice_btn);
+        incr_n_dice_btn.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) { adjustNDice(1); }
+        });
+        decr_n_dice_btn.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) { adjustNDice(-1); }
+        });
 
         return v;
     }
 
-    private void displayNDice(TextView tv){
+    private void displayNDice(TextView label){
+        TextView tv =
+                (label == null) ? (TextView) getActivity().findViewById(R.id.n_dice_label) : label;
+
         String text = "Number of Dice: " + diceManager.getNDice();
         tv.setText(text);
+    }
+
+    public void adjustNDice(int diff){
+        diceManager.adjustNDice(diff);
+        displayNDice(null);
     }
 }
