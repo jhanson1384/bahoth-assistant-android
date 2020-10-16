aae08e4ba0089f64ce09c96785b9169b8087f9f2
Jack Hanson <jhanson1384@gmail.com>
2020-10-15 19:04:49 -0500

    Created GameOverActivity, which extends MainActivity

---
+++ app/src/main/java/com/jhanson1384/bahothassistant/GameOverActivity.java
@@ -0,0 +1,13 @@
+package com.jhanson1384.bahothassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameOverActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
