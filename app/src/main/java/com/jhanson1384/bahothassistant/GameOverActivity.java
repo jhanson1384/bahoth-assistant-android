6c8bb2a323bcf7f55ce3cb505ab938d6c70ef4fa
Jack Hanson <jhanson1384@gmail.com>
2020-10-15 19:43:19 -0500

    Added "Game Over" TextView to GameOverActivity

--- app/src/main/java/com/jhanson1384/bahothassistant/GameOverActivity.java
+++ app/src/main/java/com/jhanson1384/bahothassistant/GameOverActivity.java
@@ -3,11 +3,16 @@ package com.jhanson1384.bahothassistant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class GameOverActivity extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Display "Game Over" message
+        ((TextView) findViewById(R.id.game_over_label)).setVisibility(View.VISIBLE);
     }
 }
