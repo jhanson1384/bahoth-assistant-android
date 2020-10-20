02ef9afad6e5fdbeb5972fa9c96050ab1330ae37
Jack Hanson <jhanson1384@gmail.com>
2020-10-20 18:12:37 -0500

    Added TextViews to HauntRollFragment displaying information about the roll

--- app/src/main/java/com/jhanson1384/bahothassistant/HauntRollFragment.java
+++ app/src/main/java/com/jhanson1384/bahothassistant/HauntRollFragment.java
@@ -1,5 +1,6 @@
 package com.jhanson1384.bahothassistant;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HauntRollFragment extends DiceRollFragment {
 
@@ -35,11 +38,53 @@ public class HauntRollFragment extends DiceRollFragment {
                              Bundle savedInstanceState) {
         View v = super.onCreateView(inflater, container, savedInstanceState);
 
+        //Create TextView displaying haunt count
+        initRollOverviewText((ViewGroup) v);
+
+        //Modify done_btn text
+        Button cancel_btn = (Button) v.findViewById(R.id.done_btn);
+        cancel_btn.setText("Cancel");
+
         return v;
     }
 
     @Override
     protected void initDiceManager(){
-        diceManager = new DiceManager(haunt_count);
+        diceManager = new DiceManager(6);
+    }
+
+
+    private void initRollOverviewText(ViewGroup parent_view){
+        TextView roll_overview = new TextView(getContext());
+        String message = haunt_count + " omen cards have been drawn";
+        roll_overview.setText(message);
+        roll_overview.setTextColor(Color.WHITE);
+
+        //Add TextView at top of parent
+        parent_view.addView(roll_overview, 0);
+    }
+
+    private boolean hauntHasBegun(){
+        return (diceManager.sum() < haunt_count);
+    }
+
+    @Override
+    protected void displayRollSum(){
+        TextView roll_sum = (TextView) getActivity().findViewById(R.id.roll_sum);
+        int sum = diceManager.sum();
+
+        //Set text based on roll outcome
+        String view_text = ((hauntHasBegun()) ? "Haunt has begun!\n" : "Haunt avoided.\n") +
+                            "Your roll: " + sum + "; Needed to roll: " + haunt_count;
+
+        roll_sum.setText(view_text);
+        //TextView is initially set to visibility:gone, make sure it is now visible
+        roll_sum.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void rollDice(){
+        super.rollDice();
+        //TODO
     }
 }
